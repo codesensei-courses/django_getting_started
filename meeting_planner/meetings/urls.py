b1f6e222a55ddb0d7175beb4aa8406f6a2abfe95
Reindert-Jan Ekker <info@codesensei.nl>
2020-04-19 18:44:49 +0200

    Bonus: Editing and Deleting Meetings

--- meeting_planner/meetings/urls.py
+++ meeting_planner/meetings/urls.py
@@ -6,5 +6,7 @@ from . import views
 urlpatterns = [
     path('<int:id>', views.detail, name="detail"),
     path('rooms', views.rooms_list, name="rooms"),
-    path('new', views.new, name="new")
+    path('new', views.new, name="new"),
+    path('edit/<int:id>', views.edit, name="edit"),
+    path('delete/<int:id>', views.delete, name="delete")
 ]
--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -23,3 +23,36 @@ def new(request):
     else:
         form = MeetingForm()
     return render(request, "meetings/new.html", {"form": form})
+
+
+# As a bonus, here are example implementations
+# Of an edit and delete form
+# See also urls.py and templates
+
+def edit(request, id):
+    # First, get the meeting to edit from the database
+    meeting = get_object_or_404(Meeting, pk=id)
+    if request.method == "POST":
+        # After editing: get data from form
+        # Note the second argument: the meeting we are editing
+        form = MeetingForm(request.POST, instance=meeting)
+        if form.is_valid():
+            form.save()
+            # redirect back to detail page after save
+            return redirect("detail", id)
+    else:
+        # Pre-fill the form with data from existing meeting
+        form = MeetingForm(instance=meeting)
+    return render(request, "meetings/edit.html", {"form": form})
+
+
+# Delete is different: the form is only shown to ask for confirmation
+# When we get a POST, we know we can go ahead and delete
+def delete(request, id):
+    # First, get the meeting to edit from the database
+    meeting = get_object_or_404(Meeting, pk=id)
+    if request.method == "POST":
+        meeting.delete()
+        return redirect("welcome")
+    else:
+        return render(request, "meetings/confirm_delete.html", {"meeting": meeting})
